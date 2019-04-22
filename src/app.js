69b1f25f80a7c01927a41cd111856a2185a09e85
javilobo8 <jbermudezlobo@gmail.com>
2019-04-22 23:20:20 +0200

    test

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ app.use(bodyParser.json());
 
 const channel = `#${process.env.TWITCH_CHANNEL}`;
 
-async function sendPole(body) {
+async function sendPole() {
   await tmi.join(channel);
   await tmi.say(channel, process.env.TWITCH_MESSAGE);
   await tmi.part(channel);
-  console.log('Done!');
+  console.log('Pole sent!');
 }
 
 app.get('/twitch-webhook', (req, res) => {
@@ -30,6 +30,12 @@ app.get('/twitch-webhook', (req, res) => {
 
 app.post('/twitch-webhook', (req, res) => {
   console.log(req.body);
+  if (req.body.data && Array.isArray(req.body.data) && req.body.data.length) {
+    const data = req.body.data[0];
+    if (data.type === 'live') {
+      sendPole();
+    }
+  }
   res.send('');
 });
 
